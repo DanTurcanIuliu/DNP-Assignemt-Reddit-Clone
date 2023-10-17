7c390361c339ac8353b2498ddf779f1656234247
DanTurcanIuliu <turcan.dan.ro@gmail.com>
2023-10-17 20:18:09 +0200

    Create User

---
+++ WebAPI/Controllers/UserController.cs
@@ -0,0 +1,36 @@
+﻿using Application.Logic_Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Shared.DTOs;
+using Shared.Models;
+
+namespace WebAPI.Controllers;
+
+public class UserController
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserLogic userLogic;
+
+        public UsersController(IUserLogic userLogic)
+        {
+            this.userLogic = userLogic;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<User>> CreateAsync(UserCreationDto dto)
+        {
+            try
+            {
+                User user = await userLogic.CreateAsync(dto);
+                return Created($"/users/{user.Id}", user);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return StatusCode(500, e.Message);
+            }
+        }
+    }
+}
